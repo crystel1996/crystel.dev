09c8276e5bb1c9073133a4675b7119e6b56afbf6
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-13 13:41:06 +0300

    button responsive menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 import { HeaderComponentInterface } from './interface';
 import classes from './style.module.css'
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Box, IconButton, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 import classNames from 'classnames';
 
 export const Header: FC<HeaderComponentInterface> = () => {
 
     return <Box className={classes.header}>
         <Box className={classes.headerLogo}>
-            <Typography variant='h1' className={classes.headerLogo}>CRYSTEL</Typography>
+            <Typography  className={classes.headerLogo}>CRYSTEL</Typography>
         </Box>
         <Box className={classes.headerMenu}>
             <List className={classes.headerListMenu}>
@@ -28,6 +29,13 @@ export const Header: FC<HeaderComponentInterface> = () => {
                     <ListItemText className={classes.headerListMenuitemText} >Download CV</ListItemText>
                 </ListItem>
             </List>
+
+            <Box className={classes.headerMobileMenu}>
+                <IconButton size="large" aria-label="Menu">
+                    <Menu fontSize='large' htmlColor='white' />
+                </IconButton>
+            </Box>
+
         </Box>
     </Box>
 
