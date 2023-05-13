9f258afe8fa2d433b1d3daebd756163300383ce4
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-13 13:29:03 +0300

    header meun

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,34 @@
 import React, { FC } from 'react';
 import { HeaderComponentInterface } from './interface';
-
+import classes from './style.module.css'
+import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import classNames from 'classnames';
 
 export const Header: FC<HeaderComponentInterface> = () => {
 
-    return <></>
+    return <Box className={classes.header}>
+        <Box className={classes.headerLogo}>
+            <Typography variant='h1' className={classes.headerLogo}>CRYSTEL</Typography>
+        </Box>
+        <Box className={classes.headerMenu}>
+            <List className={classes.headerListMenu}>
+                <ListItem className={classes.headerListMenuitem}>
+                    <ListItemText className={classes.headerListMenuitemText}>About</ListItemText>
+                </ListItem>
+                <ListItem className={classes.headerListMenuitem}>
+                    <ListItemText className={classes.headerListMenuitemText}>Experiences</ListItemText>
+                </ListItem>
+                <ListItem className={classes.headerListMenuitem}>
+                    <ListItemText className={classes.headerListMenuitemText}>Work</ListItemText>
+                </ListItem>
+                <ListItem className={classes.headerListMenuitem}>
+                    <ListItemText className={classes.headerListMenuitemText}>Contact</ListItemText>
+                </ListItem>
+                <ListItem className={classNames(classes.headerListMenuitem, classes.headerListMenuButton)}>
+                    <ListItemText className={classes.headerListMenuitemText} >Download CV</ListItemText>
+                </ListItem>
+            </List>
+        </Box>
+    </Box>
 
 }
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
 import { HomePageInterface } from './interface';
-
+import { Header } from '../../Components';
+import { Box } from '@mui/material';
+import classes from './style.module.css';
 
 export const HomePage: FC<HomePageInterface> = () => {
-    return <>Home</>
+    return <Box className={classes.home}>
+    
+        <Header />
+
+    </Box>
 }
