26af89207d7d01da3d56e1e57498de94dc03fa52
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-15 09:42:59 +0300

    list technologie

--- src/Components/Experiences/Exeperiences.tsx
+++ src/Components/Experiences/Exeperiences.tsx
@@ -1,37 +1,42 @@
 import React, { FC } from 'react';
 import { ExperienceComponentInterface } from './interface';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import classes from './style.module.css';
 
 export const Experience: FC<ExperienceComponentInterface> = () => {
 
     return <Box className={classes.experienceContent}>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/css3.svg" alt="Css3" />
+        <Box className={classes.experienceTitle}>
+            <Typography>Liste des technologies</Typography>
         </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/git.svg" alt="Git" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/graphql.svg" alt="Css3" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/html5.svg" alt="Css3" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/js.svg" alt="Css3" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/nestJS.svg" alt="Css3" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/nodeJS.svg" alt="Css3" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/postgresql.svg" alt="Css3" />
-        </Box>
-        <Box className={classes.experienceLogo}>
-            <img src="/Assets/react.svg" alt="Css3" />
+        <Box className={classes.experiecenceListTechno}>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/css3.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/git.svg" alt="Git" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/graphql.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/html5.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/js.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/nestJS.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/nodeJS.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/postgresql.svg" alt="Css3" />
+            </Box>
+            <Box className={classes.experienceLogo}>
+                <img src="/Assets/react.svg" alt="Css3" />
+            </Box>
         </Box>
     </Box>
 
