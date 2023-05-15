27437c405390938604a9bdb9e517d3c4ad1d8016
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-15 12:49:37 +0300

    work component

---
+++ src/Components/Work/Work.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { WorkComponentInterface } from './interface';
+
+export const WorkComponent: FC<WorkComponentInterface> = () => {
+
+    return <></>
+
+}
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,3 +2,4 @@ export * from './Header';
 export * from './Landing';
 export * from './About';
 export * from './Experiences';
+export * from './Work';
