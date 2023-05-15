770a684365344cd4f895e37bf80aead3948ae5b5
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-15 12:43:17 +0300

    Work module

---
+++ src/Pages/Work/Work.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { WorkPageInterface } from './interface';
+import classes from './style.module.css';
+import { Box } from '@mui/material';
+
+export const Work: FC<WorkPageInterface> = () => {
+
+    return <Box className={classes.home}>
+
+    </Box>
+
+}
