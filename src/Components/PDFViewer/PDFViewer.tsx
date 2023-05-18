fe2ec7cc8659ca6c0fddafa527791ac7fdf3b055
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-18 13:09:33 +0300

    download pdf

--- src/Components/PDFViewer/PDFViewer.tsx
+++ src/Components/PDFViewer/PDFViewer.tsx
@@ -2,37 +2,38 @@ import React, { FC, useEffect, useState } from 'react';
 import { PDFViewerComponentInterface } from './interface';
 import saveAs from 'file-saver';
 import { Document, Page, pdfjs } from 'react-pdf';
+import { useNavigate } from 'react-router-dom'; 
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export const PDFViewer: FC<PDFViewerComponentInterface> = () => {
+
+    const navigate = useNavigate();
 
     const pdfFilePath = "/Assets/cv.pdf";
 
     const [numPages, setNumPages] = useState<number | null>(null);
     const [pageNumber, setPageNumber] = useState<number>(1);
 
-    useEffect(() => {
-        const loadPDF = async () => {
-            try {
-                const response = await fetch(pdfFilePath);
-                const blob = await response.blob();
-                saveAs(blob, 'Developer (ReactJS / NodeJS) Crystel.pdf');
-            } catch (error) {
-                console.error('Failed to download the PDF:', error);
-            }
-        };
-
-        loadPDF();
-    }, [pdfFilePath]);
+    const loadPDF = async () => {
+        try {
+            const response = await fetch(pdfFilePath);
+            const blob = await response.blob();
+            console.log('[DOWNLOAD]')
+            saveAs(blob, 'Developer (ReactJS / NodeJS) Crystel.pdf');
+            navigate(-1);
+        } catch (error) {
+            console.error('Failed to download the PDF:', error);
+        }
+    };
 
     const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
-        setNumPages(numPages);
+        loadPDF();
     };
 
     return (
         <div>
-            <Document file={pdfFilePath} onLoadSuccess={onDocumentLoadSuccess}>
+            <Document file={pdfFilePath} onLoadSuccess={onDocumentLoadSuccess} externalLinkTarget='_blank'>
                 <Page pageNumber={pageNumber} />
             </Document>
 
