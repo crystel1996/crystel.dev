38c6ee0b7c0bb3b49d4ac81e30323dd46b0111a2
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-18 09:29:46 +0300

    work title

--- src/Components/Work/Work.tsx
+++ src/Components/Work/Work.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react';
 import { WorkComponentInterface } from './interface';
+import classes from './style.module.css'
+import { Box, Typography } from '@mui/material';
 
 export const WorkComponent: FC<WorkComponentInterface> = () => {
 
-    return <></>
+    return <Box className={classes.workContent}>
+        <Box className={classes.workTitle}>
+            <Typography>List of My Work</Typography>
+        </Box>
+    </Box>
 
 }
