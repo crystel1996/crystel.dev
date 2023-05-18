a8911122d184c970ef0b61e98ef1ed98b2657d8b
crystel1996 <crystelratsimbazafy@gmail.com>
2023-05-18 11:15:49 +0300

    list of my work item

--- src/Components/Work/Work.tsx
+++ src/Components/Work/Work.tsx
@@ -9,6 +9,76 @@ export const WorkComponent: FC<WorkComponentInterface> = () => {
         <Box className={classes.workTitle}>
             <Typography>List of My Work</Typography>
         </Box>
+        <Box className={classes.listWork}>
+            <Box className={classes.workItem}>
+                <Box className={classes.workImage}>
+                    <img src="/Assets/project-photograph-portfolio.png" alt="Portfolio photograph" />
+                </Box>
+                <Box className={classes.workBody}>
+                    <Box className={classes.workItemTitle}>
+                        <Typography variant="h4">Portfolio Photograph</Typography>
+                    </Box>
+                    <Box className={classes.workDescription}>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Link: 
+                                <Typography component="a" href="https://crystel1996.github.io/NetWedding/" target='_blank'> https://crystel1996.github.io/NetWedding/</Typography>
+                            </Typography>            
+                        </Box>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Repository: 
+                                <Typography component="a" href="https://github.com/crystel1996/NetWedding"> https://github.com/crystel1996/NetWedding</Typography>
+                            </Typography>
+                        </Box>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Techno:
+                                <Typography component="span"> ReactJS, JS, HTML5, CSS3</Typography>
+                            </Typography>
+                            
+                        </Box>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Description:
+                                <Typography component="span"> It’s a prototype of a photographer’s portfolio. </Typography>
+                            </Typography>
+                            
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+            <Box className={classes.workItem}>
+                <Box className={classes.workImage}>
+                    <img src="/Assets/my-portfolio.png" alt="My portfolio" />
+                </Box>
+                <Box className={classes.workBody}>
+                    <Box className={classes.workItemTitle}>
+                        <Typography variant="h4">My Portfolio</Typography>
+                    </Box>
+                    <Box className={classes.workDescription}>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Link: 
+                                <Typography component="a" href="https://crystel.vercel.app/" target='_blank'> https://crystel.vercel.app/</Typography>
+                            </Typography>            
+                        </Box>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Repository: 
+                                <Typography component="a" href="https://github.com/crystel1996/crystel.dev"> https://github.com/crystel1996/crystel.dev</Typography>
+                            </Typography>
+                        </Box>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Techno:
+                                <Typography component="span"> ReactJS, JS, HTML5, CSS3, Material-UI</Typography>
+                            </Typography>
+                            
+                        </Box>
+                        <Box className={classes.workDescriptionItem}>
+                            <Typography>Description:
+                                <Typography component="span"> It’s my portfolio that describes my career path. </Typography>
+                            </Typography>
+                            
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     </Box>
 
 }
